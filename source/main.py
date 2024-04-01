a6dba6f75ad67d71d38df0a1aabb4a4003e57f2d
Urban Versis 32 <107663697+UrbanVersis32@users.noreply.github.com>
2024-04-01 18:57:33 -0500

    Add basic structure for main.py
    Add basic structure for main.py

--- source/main.py
+++ source/main.py
@@ -1,3 +1,23 @@
 # KDC-DOS
+#
+# Note: Layout is as follows
+#
+# function for all commands without arguments
+# function for all commands with arguments
+# while True loop for prompt
+
+
+# Commands without argument
+def basiccommands():
+	print()
+
+# Commands with argument
+def argumentcommands():
+	print()
 
 print("Welcome to KDC-DOS!")
+
+while True:
+	prompt = input("> ")
+	if prompt == "test":
+		print("hi!")
